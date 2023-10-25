6318e38be9eddd585940c17be917ef816c9d37d6
Nusrat Mitu <nusrat.mitu@welldev.io.com>
2023-10-25 14:18:54 +0600

    added delete button

--- todo.js
+++ todo.js
@@ -5,35 +5,58 @@ const errorMessage = document.getElementById("errorMessage");
 
 let todos = [];
 
+function generateUniqueId() {
+  return Date.now();
+}
 const resetTaskInput = () => {
   taskInputText.value = "";
 };
+const isUserInputValid = (textContent) => {
+  return textContent.trim() !== "";
+};
+
 const validateInput = () => {
-  if (taskInputText.value === "") {
-    errorMessage.innerHTML = "Please add a task";
-  } else {
+  if (isUserInputValid(taskInputText.value)) {
     errorMessage.innerHTML = "";
+    return true;
+  } else {
+    errorMessage.innerHTML = "Please add a task";
+    return false;
   }
 };
+function createTaskElement(task) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+      <span class="task-text ${task.done ? "completed" : ""}">${
+    task.value
+  }</span>
+      <button class="delete-button">Delete</button>
+  `;
+  return li;
+}
 const renderTodoList = () => {
   taskList.innerHTML = "";
 
   todos.forEach((todo) => {
-    const newTask = document.createElement("li");
-    newTask.innerHTML = `${todo}`;
+    const newTask = createTaskElement(todo);
     taskList.appendChild(newTask);
   });
+  console.log(todos);
 };
 
 const handleCreateTodo = () => {
   const textContent = taskInputText.value;
 
-  if (textContent !== "") {
-    todos.push(textContent);
+  if (validateInput()) {
+    const taskId = generateUniqueId();
+    const task = { id: taskId, value: textContent, done: false };
+    todos.push(task);
     errorMessage.innerHTML = "";
     renderTodoList();
     resetTaskInput();
-  } else validateInput();
+  }
 };
+
+console.log(todos);
 addTaskBtn.addEventListener("click", handleCreateTodo);
 taskInputText.addEventListener("input", validateInput);
