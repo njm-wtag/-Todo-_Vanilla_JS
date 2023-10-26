b1bba83658d25fd6d18453d7109183f888b4c4d8
Nusrat Mitu <nusrat.mitu@welldev.io.com>
2023-10-26 16:23:25 +0600

    resolved typo and formatting

--- todo.js
+++ todo.js
@@ -24,16 +24,20 @@ const validateInput = () => {
   errorMessage.innerHTML = "Please add a task";
   return false;
 };
+
 const handleDelete = (taskId) => {
   todos = todos.filter((todo) => todo.id !== taskId);
   renderTodoList();
 };
+
 function createDeleteButton(taskId) {
-  const deteleButton = document.createElement("button");
-  deteleButton.textContent = "Delete";
-  deteleButton.addEventListener("click", () => handleDelete(taskId));
-  return deteleButton;
+  constdeleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => handleDelete(taskId));
+
+  return deleteButton;
 }
+
 function createTaskElement(task) {
   const li = document.createElement("li");
   li.innerHTML = `
@@ -41,6 +45,7 @@ function createTaskElement(task) {
   `;
   const deleteButton = createDeleteButton(task.id);
   li.appendChild(deleteButton);
+
   return li;
 }
 const renderTodoList = () => {
